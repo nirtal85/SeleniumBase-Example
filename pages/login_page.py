3163bd1bfe3c7e1d01cd09f85417603501f3efd7
unknown <eshorrosh@gmail.com>
2020-11-21 22:03:35 +0200

    try pr

---
+++ pages/login_page.py
@@ -0,0 +1,7 @@
+class LoginPage:
+    page_title = "//h2[text()='Login Page']"
+    username_field = "#username"
+    password_field = "#password"
+    login_button = "[type='submit']"
+    message = "#flash"
+    logout_button = ".button.secondary.radius"
