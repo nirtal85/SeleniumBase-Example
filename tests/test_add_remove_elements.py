674cf020d86856b07e0623f627274c3c61140ce0
unknown <eshorrosh@gmail.com>
2021-01-02 22:09:22 +0200

    click link text implementation

--- tests/test_add_remove_elements.py
+++ tests/test_add_remove_elements.py
@@ -8,16 +8,16 @@ class TestAddRemoveElements(BaseTestCase):
 
     @allure.description("Add new element to page")
     def test_add_element(self):
-        self.click(MainPage.add_remove_elements)
+        self.click_link_text(MainPage.add_remove_elements)
         self.assert_element(addrem.add_element_button)
         self.click(addrem.add_element_button)
         self.assert_element_visible(addrem.delete_added_elements_buttons_list)
         link_list = self.find_elements(addrem.delete_added_elements_buttons_list)
-        self.assert_true(len(link_list) == 0)
+        self.assert_true(len(link_list) == 1)
 
     @allure.description("Delete an added element from page")
     def test_delete_element(self):
-        self.click(MainPage.add_remove_elements)
+        self.click_link_text(MainPage.add_remove_elements)
         self.assert_element(addrem.add_element_button)
         self.click(addrem.add_element_button)
         self.click(addrem.delete_added_element)
