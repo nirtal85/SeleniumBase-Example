cdb482d066a9ea9f3bf6e4a6a8c1cc0d809041b2
unknown <eshorrosh@gmail.com>
2021-02-13 15:20:52 +0200

    Fix parametrize issue

--- tests/test_drop_down.py
+++ tests/test_drop_down.py
@@ -4,15 +4,14 @@ from selenium.webdriver.common.by import By
 
 from pages.drop_down_page import DropDown
 from pages.main_page import MainPage
-from tests.base_test import BaseTestCase
 
 
-class TestDropDown(BaseTestCase):
+class TestDropDown:
 
     @allure.description("Select first option in drop down")
     @mark.parametrize("option_index", [0, 1])
-    def test_select_option_one(self, option_index):
-        self.click_link_text(MainPage.drop_down)
-        self.assert_element_visible(DropDown.page_title, By.XPATH)
-        self.select_option_by_text(DropDown.drop_down, DropDown.options_text[option_index])
-        self.assert_text_visible(DropDown.options_text[option_index], DropDown.drop_down)
+    def test_select_option_one(self, sb, option_index):
+        sb.click_link_text(MainPage.drop_down)
+        sb.assert_element_visible(DropDown.page_title, By.XPATH)
+        sb.select_option_by_text(DropDown.drop_down, DropDown.options_text[option_index])
+        sb.assert_text_visible(DropDown.options_text[option_index], DropDown.drop_down)
