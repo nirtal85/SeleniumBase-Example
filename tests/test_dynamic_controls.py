fac1a8c211ebb1c7c680165ea688e6d0fabef06e
unknown <eshorrosh@gmail.com>
2021-02-13 15:03:45 +0200

    fix parametrization

--- tests/test_dynamic_controls.py
+++ tests/test_dynamic_controls.py
@@ -2,22 +2,21 @@ from pytest import mark
 
 from pages.dynamic_controls_page import DynamicControls
 from pages.main_page import MainPage
-from tests.base_test import BaseTestCase
 
 
-class TestDynamicControls(BaseTestCase):
+class TestDynamicControls:
 
-    @mark.parametrize("action", ["remove,add"])
-    def test_remove_add_checkbox(self, action):
-        self.click_link_text(MainPage.dynamic_controls)
-        self.assert_element_visible(DynamicControls.page_title)
-        self.click(DynamicControls.add_remove_checkbox_button)
+    @mark.parametrize("action", ["remove", "add"])
+    def test_remove_add_checkbox(self, sb, action):
+        sb.click_link_text(MainPage.dynamic_controls)
+        sb.assert_element_visible(DynamicControls.page_title)
+        sb.click(DynamicControls.add_remove_checkbox_button)
         if action == "remove":
-            self.assert_element_not_visible(DynamicControls.check_box)
-            self.assert_text_visible("It's gone!", DynamicControls.success_message)
+            sb.assert_element_not_visible(DynamicControls.check_box)
+            sb.assert_text_visible("It's gone!", DynamicControls.success_message)
         elif action == "add":
-            self.assert_element_visible(DynamicControls.check_box)
-            self.assert_text_visible("It's back!", DynamicControls.success_message)
+            sb.assert_element_visible(DynamicControls.check_box)
+            sb.assert_text_visible("It's back!", DynamicControls.success_message)
 
     @mark.parametrize("action", ["enable", "disable"])
     def test_enable_disable_text_field(self, action):
