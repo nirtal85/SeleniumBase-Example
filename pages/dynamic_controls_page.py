bad36810df28eea37c560b9ead44a12da3283fb6
unknown <eshorrosh@gmail.com>
2021-02-13 14:20:43 +0200

    Add dynamic controls tests

---
+++ pages/dynamic_controls_page.py
@@ -0,0 +1,7 @@
+class DynamicControls:
+    page_title = ".example > h4:nth-child(1)"
+    check_box = "[type='checkbox']"
+    add_remove_checkbox_button = "#checkbox-example [type='button']"
+    enable_disable_field = "[type='text']"
+    enable_disable_button = "#input-example [type='button']"
+    success_message = "p#message"
